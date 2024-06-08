34dd44c259accb9fe19fc711c084889306b69911
wildhevire <wildhevire404@gmail.com>
2024-06-09 01:00:44 +0700

    pywal

---
+++ hypr/scripts/pywal.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+wallpaper=$(readlink "$HOME/Pictures/current_wallpaper")
+
+
+check_file() {
+   
+  if [ ! -f "$1" ]; then
+    echo "File $1 not found!"
+    exit 1
+  fi
+}
+
+check_file "$wallpaper"
+
+
+wal -i "$wallpaper"
+dunstify -a "Change color" "Colorchanged" 
