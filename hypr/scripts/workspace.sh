8f382b83bb2312fd34446eb20104819f58437888
wildhevire <wildhevire404@gmail.com>
2024-06-16 21:35:31 +0700

    hyprland input config

--- hypr/scripts/workspace.sh
+++ hypr/scripts/workspace.sh
@@ -5,6 +5,11 @@ DEVICE=$(cat /sys/devices/virtual/dmi/id/product_family)
 
 if [[ $DEVICE == "Swift 3" ]]; then
     ln -sf ~/.config/hypr/workspace_laptop.conf ~/.config/hypr/workspace.conf
+    ln -sf ~/.config/hypr/input_laptop.conf ~/.config/hypr/input.conf
+    ln -sf ~/.config/hypr/env_laptop.conf ~/.config/hypr/env.conf
+    
 else
     ln -sf ~/.config/hypr/workspace_desktop.conf ~/.config/hypr/workspace.conf
+    ln -sf ~/.config/hypr/input_desktop.conf ~/.config/hypr/input.conf
+    ln -sf ~/.config/hypr/env_desktop.conf ~/.config/hypr/env.conf
 fi
